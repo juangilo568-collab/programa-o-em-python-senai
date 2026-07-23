82eed1e60f84e72619faf8e2cbba98370fb2c6e2
juangilo568-collab <juangilo568@gmail.com>
2026-07-23 19:40:56 -0300

    Add files via upload

---
+++ Aula-6/Aula 6/main.py
@@ -0,0 +1,30 @@
+#Calcule o módulo ( % ) de 17 por 4.
+print(71/4)
+
+#Multiplique 9 por 3 e, em seguida, eleve o resultado ao quadrado.
+print((9*3)** 2)
+
+#Calcule a raiz quadrada de 81.
+print(81 ** 0.5)
+
+#dicione 20 a 3 vezes 4.
+print(20 + (3*4))
+
+#Multiplique 15 por 2 e, em seguida, subtraia 7.
+print((15*2) - 7)
+
+#Eleve 5 ao cubo.
+print(5**3)
+
+#Calcule a média de 17, 21 e 25.
+media = (17+21+25) / 3
+print(media)
+
+#Multiplique 11 por 2 e adicione 7.
+print((11*2) + 7)
+
+#Subtraia 15 de 3 vezes 8 e divida o resultado por 2.
+print(((3*8) - 15) / 2)
+
+#Eleve 2 à potência de 10. 
+print(2 ** 10)
